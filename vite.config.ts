368dda3b6b2ef270a7a7eaef386bcf46b861c05d
Zoniaina_Teddy <zoniainateddy@gmail.com>
2026-01-31 11:48:46 +0300

    build 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Todo_app/',
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
   ],
-  server: {
-    // Pour le développement, redirige / vers /portfolio
-    proxy: {
-      '/': {
-        target: 'https://teds81.github.io/',
-        changeOrigin: true,
-        rewrite: (path) => path === '/' ? '/Todo_app/' : path
-      }
-    }
-  }
 })
